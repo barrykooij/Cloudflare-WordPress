f562cb75012a1a336df74eae9c98aed9d6b53632
Barry Kooij <barrykooij@users.noreply.github.com>
2026-09-23 08:37:27 +0200

    Fixed: deprecated CF classes accepting constructor arguments their parent classes reject

--- deprecated/WordPress/ClientActions.php
+++ deprecated/WordPress/ClientActions.php
@@ -5,7 +5,7 @@ namespace CF\WordPress;
  * @deprecated 4.13.0 Use \Cloudflare\APO\WordPress\ClientActions instead.
  */
 class ClientActions extends \Cloudflare\APO\WordPress\ClientActions {
-    public function __construct(\Cloudflare\APO\Integration\IntegrationInterface $integrationContext, \Cloudflare\APO\API\APIInterface $api, \Cloudflare\APO\API\Request $request) {
+    public function __construct(\Cloudflare\APO\Integration\DefaultIntegration $integrationContext, \Cloudflare\APO\API\APIInterface $api, \Cloudflare\APO\API\Request $request) {
         _deprecated_file( __FILE__, '4.13.0', "Cloudflare\APO\WordPress\ClientActions", 'CF\WordPress namespace is deprecated, use Cloudflare\APO\WordPress instead.' );
         parent::__construct($integrationContext, $api, $request);
     }
--- deprecated/WordPress/WordPressAPI.php
+++ deprecated/WordPress/WordPressAPI.php
@@ -5,7 +5,7 @@ namespace CF\WordPress;
  * @deprecated 4.13.0 Use \Cloudflare\APO\WordPress\WordPressAPI instead.
  */
 class WordPressAPI extends \Cloudflare\APO\WordPress\WordPressAPI {
-    public function __construct(\Cloudflare\APO\Integration\DataStoreInterface $dataStore) {
+    public function __construct(\Cloudflare\APO\WordPress\DataStore $dataStore) {
         _deprecated_file( __FILE__, '4.13.0', "Cloudflare\APO\WordPress\WordPressAPI", 'CF\WordPress namespace is deprecated, use Cloudflare\APO\WordPress instead.' );
         parent::__construct($dataStore);
     }
